c31c3e4c3adaccda0ae7abc68967f496d4985a06
Tobias Theobald <tobias@tobi042.de>
2015-01-26 12:22:10 +0100

    Wifi Channels verändert, Version angepasst

--- site.mk
+++ site.mk
@@ -32,7 +32,7 @@ GLUON_SITE_PACKAGES := \
 #		gluon relies on
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
-DEFAULT_GLUON_RELEASE := 0.1-exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.2-exp$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
