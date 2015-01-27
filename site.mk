aeb78948ba82fe74ce5d42f8a58532391ad3cac1
Tobias Theobald <tobias@tobi042.de>
2015-01-27 21:57:32 +0100

    probieren

--- site.mk
+++ site.mk
@@ -3,28 +3,27 @@
 ##	GLUON_SITE_PACKAGES
 #		specify gluon/openwrt packages to include here
 GLUON_SITE_PACKAGES := \
-  gluon-mesh-batman-adv-14 \
-  gluon-alfred \
-  gluon-announced \
-  gluon-autoupdater \
-  gluon-ebtables-filter-multicast \
-  gluon-ebtables-filter-ra-dhcp \
-  gluon-luci-admin \
-  gluon-luci-autoupdater \
-  gluon-luci-portconfig \
-  gluon-next-node \
-  gluon-mesh-vpn-fastd \
-  gluon-radvd \
-  gluon-status-page \
-  iwinfo \
-  iptables \
-  haveged \
-  gluon-config-mode-autoupdater \
-  gluon-config-mode-hostname \
-  gluon-config-mode-mesh-vpn \
-  gluon-config-mode-geo-location \
-  gluon-config-mode-contact-info
-
+	gluon-mesh-batman-adv-14 \
+	gluon-alfred \
+	gluon-announced \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-contact-info \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-status-page \
+	iwinfo \
+	iptables \
+	haveged
 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
