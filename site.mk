435cca0a7d3ed544f73947ebb9ec2f07a19a79a5
Ralf Jung <post@ralfj.de>
2016-05-28 14:18:03 +0200

    this is the 1.3 experimental series

--- site.mk
+++ site.mk
@@ -37,7 +37,7 @@ GLUON_SITE_PACKAGES := \
 #		gluon relies on
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
-DEFAULT_GLUON_RELEASE := 1.2~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 1.3~exp$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
