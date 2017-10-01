e1d4e7f09598bddc58d5d02b1555a43b9686d77a
Ralf Jung <post@ralfj.de>
2017-10-01 16:40:31 +0200

    2nd beta, first was broken

--- version.sh
+++ version.sh
@@ -7,9 +7,10 @@ export GLUON_UPSTREAM_TAG="v2017.1.2"
 
 # This is where we control the version number of our firmware.
 CURRENT_VERSION="1.5.0"
-CURRENT_BETA_SUFFIX="~rc1"
+CURRENT_BETA_SUFFIX="~rc2"
 CURRENT_EXPERIMENTAL_SUFFIX="~exp$(date '+%Y%m%d')"
 
+###################################################################################################
 ## Do NOT change anything below here!
 # Now we compute the actual version number.
 
