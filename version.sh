906be21d9963631a0a86097e1058c38862efcbf8
Ralf Jung <post@ralfj.de>
2017-10-12 09:08:47 +0200

    increment version

--- version.sh
+++ version.sh
@@ -6,7 +6,7 @@
 export GLUON_UPSTREAM_TAG="v2017.1.3"
 
 # This is where we control the version number of our firmware.
-CURRENT_VERSION="1.5.0"
+CURRENT_VERSION="1.5.1"
 CURRENT_BETA_SUFFIX="~rc2"
 CURRENT_EXPERIMENTAL_SUFFIX="~exp$(date '+%Y%m%d')"
 
