74ee75acf08ec504e5fee635b1523919b3e66ac2
Ralf Jung <post@ralfj.de>
2020-01-08 22:06:13 +0100

    upgrade gluon, reset and bump versions

--- version.sh
+++ version.sh
@@ -3,11 +3,11 @@
 # $1 is used to determine whether we use an experimental, beta, or stable version
 
 # This is where we control which Gluon version to use.
-export GLUON_UPSTREAM_TAG="v2019.1"
+export GLUON_UPSTREAM_TAG="v2019.1.1"
 
 # This is where we control the version number of our firmware.
-CURRENT_VERSION="1.8.0"
-CURRENT_BETA_SUFFIX="~rc2"
+CURRENT_VERSION="1.8.1"
+CURRENT_BETA_SUFFIX="~rc1"
 CURRENT_EXPERIMENTAL_SUFFIX="~exp$(date '+%Y%m%d')"
 
 ###################################################################################################
