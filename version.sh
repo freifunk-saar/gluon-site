efc0395478450799b6b465bf22d5a8444df444cd
Ralf Jung <post@ralfj.de>
2020-02-05 19:23:09 +0100

    bump to gluon 2019.1.2

--- version.sh
+++ version.sh
@@ -3,7 +3,7 @@
 # $1 is used to determine whether we use an experimental, beta, or stable version
 
 # This is where we control which Gluon version to use.
-export GLUON_UPSTREAM_TAG="v2019.1.1"
+export GLUON_UPSTREAM_TAG="v2019.1.2"
 
 # This is where we control the version number of our firmware.
 CURRENT_VERSION="1.8.1"
